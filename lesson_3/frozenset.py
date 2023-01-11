ea4482d4b3d7f1f3afd32e395e92a67b5643b420
EugeniRosh <eugeneroshenko@yndex.ru>
2023-01-11 19:40:44 +0300

    прверял оперции множества frozenset

---
+++ lesson_3/frozenset.py
@@ -0,0 +1,15 @@
+frozenset_1 = {'25'}
+
+print(frozenset_1)
+
+frozenset_2 = frozenset('521583231')
+
+print(frozenset_2)
+
+frozenset_3 = frozenset((1,))
+
+print(frozenset_3)
+
+
+
+print(id(frozenset_2))
