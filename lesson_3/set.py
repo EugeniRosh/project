febe8c649bc9d811378a7f9bfe19d668d6c653c3
EugeniRosh <eugeneroshenko@yndex.ru>
2023-01-11 19:04:34 +0300

    прверял оперции множества set

---
+++ lesson_3/set.py
@@ -0,0 +1,37 @@
+set_1 = ['hello', 'daddy', 'mum', 'hello']
+
+print(set(set_1))
+
+set_2 = set('heLloworLd')
+
+print(set_2)
+
+set_3 = {'a', 'b', 'b', 'd', 'e', 'hello', 'hello', 'world', 'dog'}
+
+print(set_3)
+
+set_3.add('z')
+
+set_3.update('hello')
+
+set_3.add(1)
+
+print(set_3)
+
+set_3.discard(1)
+
+print(set_3)
+
+set_3.remove('b')
+
+set_3.difference_update('hello')
+
+print(set_3)
+
+print(set_3.pop())
+
+print(set_3)
+
+set_3.clear()
+
+print(set_3)
