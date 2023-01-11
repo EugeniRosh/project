8f2b214e439b781d76dc983659d0046f499dd7f4
EugeniRosh <eugeneroshenko@yndex.ru>
2023-01-11 19:20:15 +0300

    прверял оперции множества tuple и изменил его

---
+++ lesson_3/tuple.py
@@ -0,0 +1,17 @@
+tuple_1 = (1, 2, 3, 4, 5, 5, 5,)
+
+print(tuple_1.count(5))
+
+print(tuple_1.index(3))
+
+print(tuple_1.index(5))
+
+print(tuple_1)
+
+list_1 = list(tuple_1)
+
+list_1.append(10)
+
+tuple_1 = tuple(list_1)
+
+print(tuple_1)
