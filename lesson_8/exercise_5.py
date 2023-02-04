00caad7956b6d0365f45a6566ff9a0a927273dd1
EugeniRosh <eugeneroshenko@yndex.ru>
2023-02-04 14:32:14 +0300

    Из csv-файла перенес данные в xlsx-файл

---
+++ lesson_8/exercise_5.py
@@ -0,0 +1,19 @@
+﻿"""Прочитать сохранённый csv-файл и сохранить данные в excel-файл,
+ кроме возраста - столбец с этими данными не нужен"""
+
+import csv
+import openpyxl
+
+with open('csv_file.csv', 'r', encoding='utf=8') as file:
+    rows = csv.reader(file)
+    book = openpyxl.Workbook()
+    sheet = book.active
+    num = 1
+    for row in rows:
+        sheet[f'B{num}'].value = row[0]
+        sheet[f'C{num}'].value = row[1]
+        sheet[f'D{num}'].value = row[3]
+        sheet[f'A{num + 1}'].value = f'Persona {num}'
+        num += 1
+    book.save('xlsx_file.xlsx')
+    book.close()
