d30369307bb4625dae41ac6c83d54a9e9f4a36bc
EugeniRosh <eugeneroshenko@yndex.ru>
2023-02-06 20:29:58 +0300

    заменил код на использование методов

--- lesson_8/exercise_5.py
+++ lesson_8/exercise_5.py
@@ -8,12 +8,11 @@ with open('csv_file.csv', 'r', encoding='utf=8') as file:
     rows = csv.reader(file)
     book = openpyxl.Workbook()
     sheet = book.active
-    num = 1
+    num = 0
     for row in rows:
-        sheet[f'B{num}'].value = row[0]
-        sheet[f'C{num}'].value = row[1]
-        sheet[f'D{num}'].value = row[3]
-        sheet[f'A{num + 1}'].value = f'Persona {num}'
+        row.pop(2)
+        row.insert(0, f'Persona {num}') if num != 0 else row.insert(0, f'')
+        sheet.append(row)
         num += 1
     book.save('xlsx_file.xlsx')
     book.close()
