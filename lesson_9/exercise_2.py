3545f8c976183194673d3e9a557f3e19c322c56b
EugeniRosh <eugeneroshenko@yndex.ru>
2023-02-08 17:45:51 +0300

    соеденил 2 списка в один

---
+++ lesson_9/exercise_2.py
@@ -0,0 +1,13 @@
+﻿"""Модуль соеденяющий 2 списка"""
+first_dict = {'a': 1, 'b': 2, 'c': 3}
+
+second_dict = {'c': 3, 'd': 4, 'e': 5}
+
+merged_dict = {}
+
+for k in first_dict.keys():
+    for i in second_dict.keys():
+        merged_dict.setdefault(k, [first_dict.get(k), second_dict.get(k)])
+        merged_dict.setdefault(i, [first_dict.get(i), second_dict.get(i)])
+
+print(merged_dict)
