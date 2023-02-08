f7bc19ad2ca8ab6fc3d753d2c53fa27d7ed53ff9
EugeniRosh <eugeneroshenko@yndex.ru>
2023-02-08 17:47:03 +0300

    подсчитал количество букв в текстовом файле

---
+++ lesson_9/exercise_3.py
@@ -0,0 +1,14 @@
+﻿"""Модуль подсчитывающий количество букв в тексте не зависимо от регистра"""
+with open('text.txt', 'r', encoding='utf=8') as file:
+    text = file.read()
+
+
+def letter_count(letter: str) -> None:
+    counter = 0
+    for i in range(len(text)):
+        if letter.lower() == text[i] or letter.upper() == text[i]:
+            counter += 1
+    print(f'Результат: буква встречается {counter} раза в тексте.')
+
+
+letter_count(input('Введине латинскую букву: '))
