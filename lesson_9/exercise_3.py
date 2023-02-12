817e6d4857c2f4e43a30d7305e2b874834b1ed0b
EugeniRosh <eugeneroshenko@yndex.ru>
2023-02-12 21:00:40 +0300

    изменил на построчное чтение файла

--- lesson_9/exercise_3.py
+++ lesson_9/exercise_3.py
@@ -1,14 +1,14 @@
-﻿"""Модуль подсчитывающий количество букв в тексте не зависимо от регистра"""
-with open('text.txt', 'r', encoding='utf=8') as file:
-    text = file.read()
+﻿"""A module that counts the number of letters in a text, regardless of case"""
 
 
 def letter_count(letter: str) -> None:
     counter = 0
-    for i in range(len(text)):
-        if letter.lower() == text[i] or letter.upper() == text[i]:
-            counter += 1
-    print(f'Результат: буква встречается {counter} раза в тексте.')
+    with open('text.txt', 'r', encoding='utf=8') as file:
+        for line in file:
+            for i in range(len(line)):
+                if letter.lower() == line[i] or letter.upper() == line[i]:
+                    counter += 1
+        print(f'Результат: буква встречается {counter} раза в тексте.')
 
 
 letter_count(input('Введине латинскую букву: '))
